56f5e623c01c415036c49442ed3ea960025f64e9
Kirill Chebulaev <chebulaev@gmail.com>
2022-06-28 18:59:29 +0300

    code cleanup

--- src/test/java/org/mytests/tests/test/LoginTest.java
+++ src/test/java/org/mytests/tests/test/LoginTest.java
@@ -1,6 +1,6 @@
 package org.mytests.tests.test;
 
-import com.jdiai.tools.Timer;
+import com.epam.jdi.light.elements.common.Alerts;
 import org.hamcrest.Matchers;
 import org.mytests.steps.DashboardSteps;
 import org.mytests.steps.LoginSteps;
@@ -8,11 +8,14 @@ import org.mytests.steps.ProjectConfigurationSteps;
 import org.mytests.tests.TestsInit;
 import org.mytests.tests.testng.TestNGListener;
 import org.mytests.utils.Project;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.mytests.utils.User;
 
+import static com.jdiai.tools.Timer.waitCondition;
 import static org.mytests.uiobjects.example.site.SiteJdi.dashboardPage;
 import static org.mytests.uiobjects.example.site.SiteJdi.landingPage;
 import static org.mytests.uiobjects.example.site.SiteJdi.projectConfigurationPage;
@@ -21,16 +24,17 @@ import static org.mytests.uiobjects.example.site.SiteJdi.projectConfigurationPag
 @Listeners(TestNGListener.class)
 public class LoginTest implements TestsInit {
 
+    ThreadLocal<Project<Project.Manual>> project = new ThreadLocal<>();
     @BeforeMethod
     public void before() {
         landingPage.open();
         landingPage.shouldBeOpened();
+        project.set(Project.defaultProject());
     }
 
     @Test
     public void test() {
         User user = User.getUser("admin");
-        Project<Project.Manual> project = Project.defaultProject();
         landingPage.getSignButton().has()
                 .text(Matchers.containsString("Sign in with your Cirro account"));
         landingPage.getSignButton().click();
@@ -48,33 +52,48 @@ public class LoginTest implements TestsInit {
         projectConfigurationPage.shouldBeOpened();
         projectConfigurationPage.getSideBar().getProjectInfo().is().displayed();
 
-        ProjectConfigurationSteps.projectSetupFill(project);
+        ProjectConfigurationSteps.projectSetupFill(project.get());
         ProjectConfigurationSteps.clickSubmitButton();
 
-        ProjectConfigurationSteps.projectGeneralFill(project);
+        ProjectConfigurationSteps.projectGeneralFill(project.get());
         ProjectConfigurationSteps.clickSubmitButton();
 
-        ProjectConfigurationSteps.projectFlexFill(project);
+        ProjectConfigurationSteps.projectFlexFill(project.get());
         ProjectConfigurationSteps.clickSubmitButton();
 
         projectConfigurationPage.getAlertMessage().is().displayed().and().has().cssClass("alert-info")
                 .and().has().text(Matchers.containsString("Project was successfully updated"));
-        projectConfigurationPage.getProjectMenu().getMenuItem(project.getName(), false).is().displayed();
+        projectConfigurationPage.getProjectMenu().getMenuItem(project.get().getName(), false).is().displayed();
         projectConfigurationPage.getProjectMenu().collapseMenu();
 
         //to be refactored
-        ProjectConfigurationSteps.projectTeamFill(project);
+        ProjectConfigurationSteps.projectTeamFill(project.get());
         ProjectConfigurationSteps.clickContinueButton();
 
         //to be refactored
-        ProjectConfigurationSteps.projectTestCaseManualFill(project);
+        ProjectConfigurationSteps.projectTestCaseManualFill(project.get());
         ProjectConfigurationSteps.clickSubmitButton();
 
-        Timer.waitCondition(() -> dashboardPage.getAlertMessage().isDisplayed());
+        waitCondition(() -> dashboardPage.getAlertMessage().isDisplayed());
         dashboardPage.getAlertMessage().is().displayed().and().has().cssClass("alert-info")
                         .and().has().text(Matchers.containsString("Test manager connection was successfully added"));
 
-        LoginSteps.signOut();
 
+
+    }
+
+    @AfterMethod
+    public void cleanup(ITestResult result) {
+        if (result.isSuccess()) { //would cleanup only if test passes
+            dashboardPage.open();
+            dashboardPage.getProjectMenu().getMenuItem(project.get().getName(), false).click();
+            waitCondition(() -> projectConfigurationPage.getSideBar().isDisplayed());
+            projectConfigurationPage.getSideBar().clickMenuItem("Configuration");
+            waitCondition(() -> projectConfigurationPage.getSubmitButton().isDisplayed());
+            projectConfigurationPage.getSubmitButton().click();
+            Alerts.acceptAlert();
+            waitCondition(() -> dashboardPage.getAlertMessage().text().contains("Project was successfully destroyed"));
+        }
+        LoginSteps.signOut();
     }
 }
